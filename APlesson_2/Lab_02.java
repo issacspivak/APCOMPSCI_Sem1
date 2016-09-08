478a1c56d20a7ecb7bcf28049f0242ad517937c5
issacspivak <issacspivak@gmail.com>
2016-09-08 11:09:15 -0700

    Added lesson 2

--- APlesson_2/Lab_02.java
+++ APlesson_2/Lab_02.java
@@ -6,6 +6,20 @@ public class Lab_02
 		int y = 5;
 		int mult = x * y;
 		
-		System.out.println(x + (" " + "Multiplied by" + y + " " + "is" + " ") mult);
+		System.out.println(x + " " + "Multiplied by" + " " + y + " " + "is" + " " + mult);
+		
+		String name = "Issac Spivak";
+		String address = "1234 Torrey Rd";
+		String city = "San Diego";
+		int zip = 92014;
+		
+		System.out.println(name + ", " + address);
+		System.out.println(city + ", " + zip);
+		
+		int l = 13;
+		int w = 2;
+		int h = 2;
+		
+		System.out.println("The surface area of your rectangle is " + (l * w * h));
 	}
 }
