a1e21459ad67462e8c1d90272dd56b105bfa0cc0
issacspivak <issacspivak@gmail.com>
2016-09-08 10:44:33 -0700

    Name change

---
+++ APlesson_2/Lab_02.java
@@ -0,0 +1,11 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 8;
+		int y = 5;
+		int mult = x * y;
+		
+		System.out.println(x + (" " + "Multiplied by" + y + " " + "is" + " ") mult);
+	}
+}
