efb5efdedbcaea165416725752c694963c05cd5b
issacspivak <issacspivak@gmail.com>
2016-10-24 12:12:54 -0700

    ex5

--- APlesson_5/Lesson_05_ex5.java
+++ APlesson_5/Lesson_05_ex5.java
@@ -3,30 +3,60 @@ public class Lesson_05_ex5
 {
 	public static void main(String[]args)
 	{
-		if(choice == choice1)\
-		{
-			if (choice == a)
-			{
-				if (choice == b)
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("You are sitting at home watching TV, then you get a call from a random number."
+		+ "\n Do you answer?" + "\n Answer 1 for yes" + "\n Answer 2 for no");
+		int choice1 = keyboard.nextInt();
+		
+		if(choice1 == 1)
+			System.out.println("You answer, all you hear is laughing and gunshots."
+			+ "\n Do you stay on the phone?" + "\n Answer 1 for yes" + "\n or 2 for no");
+			int choice2 = keyboard.nextInt();
+			
+			if (choice2 == 1)
+				System.out.println("Im coming for you, says the voice."
+				+ "\n You run to look outside all the windows but theres nothing there" + "Still wanna stay on the phone?"
+				+ "\n Answer 1 for yes" + "\n or 2 for no");
+				int choice3 = keyboard.nextInt();
+				
+				if (choice3 == 1)
+					System.out.println("The voice says... Dont you wish you had a gun for moments like this?"
+					+ "Come down to Daves guns and ammo so you never have to feel scared. In complete shock and relief, you hang up the phone and go back to watching TV");
 				
 				else
-			}
+					System.out.println("You hang up and go back to watching TV, but your sweating and shaking a bit. Should have never taken the call");
+			
 			
 			else
-		}
+				System.println("You hang up the phone. But you start to worry about why they called you"
+				+ "\n Do you google the number?"
+				+ "\n Answer 1 for yes" + "\n or 2 for no");
+				int choice4 = keyboard.nextInt;
+				
+				if (choice4 == 1)
+					System.out.println("You look it up and see its a gun store. Then you faint because your so frightened");
+				else
+		
 		else
-			if (choice == a)
-			{
-				if (choice == b)
-				{
+			System.out.println("You hang up. But it calls again."
+			+ "\n Do you just answer to see whats up?" + "\n Answer 1 for yes" + "\n or 2 for no");
+			int choice5 = keyboard.nextInt
+			
+			if (choice5 == 1)
+				System.out.println("You answer, all you hear is laughing and gunshots."
+				+ "\n Do you stay on the phone?" + "\n Answer 1 for yes" + "\n or 2 for no");
+				int choice6 = keyboard.nextInt();
+				
+				if (choice6 == 1)
+					System.out.println("Im coming for you, says the voice."
+					+ "\n You run to look outside all the windows but theres nothing there. You hang up in regret you picked up the second time");
+				
 					
-				}
+				else if
 				
-				else
-				
-			}
-			else
-				if (choice == c)
-				else
+			
+			//else if
+				//if (choice == c)
+				//else
 	}
 }
