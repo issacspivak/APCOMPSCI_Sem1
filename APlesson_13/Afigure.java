5b681817f2a354b8b772df5940c928cb3a518c87
issacspivak <issacspivak@gmail.com>
2017-02-08 12:10:44 -0800

    lesson 13

---
+++ APlesson_13/Afigure.java
@@ -0,0 +1,21 @@
+public class Afigure extends Toy
+{
+	Afigure object = new Afigure();
+	
+	public Afigure()
+	{
+		super();
+	}
+	
+	public Afigure(String n)
+	{
+		super(n);
+	}
+	
+	public String getType()
+	{
+		return "Action Figure";
+	}
+	
+	
+}
