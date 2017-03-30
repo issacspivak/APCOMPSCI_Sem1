36eed259bd28717c4f2901ca494cca9bfc83afbf
issacspivak <issacspivak@gmail.com>
2017-03-30 10:43:32 -0700

    13 and 14

--- APlesson_13/ToyRunner.java
+++ APlesson_13/ToyRunner.java
@@ -3,9 +3,9 @@ public class ToyRunner
 	public static void main(String[]args)
 	{
 		Afigure object1 = new Afigure("GI Joe");
-		Car object2 = new Car("Car");
+		System.out.println(object1.toString());
 		
-		System.out.println(object1);
-		System.out.println(object2);
+		Car object2 = new Car("Mercedes");
+		System.out.println(object2.toString());
 	}
 }
