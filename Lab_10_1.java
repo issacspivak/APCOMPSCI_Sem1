d76dbb92f66474bb53e04bb69d27d8f183086e1f
issacspivak <issacspivak@gmail.com>
2017-04-18 01:55:53 -0700

    finished 13.2

--- Lab_10_1.java
+++ Lab_10_1.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
-<<<<<<< HEAD
 
-public class Lab_10_ex1
+public class Lab_10_1
 {
     public static void main(String[] args)
     {
@@ -11,6 +10,7 @@ public class Lab_10_ex1
         ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
 		
         System.out.println(nums);
+        
         removePrimes(nums);
         System.out.println(nums);        
         
@@ -43,51 +43,3 @@ public class Lab_10_ex1
         
     }
 }
-	
-=======
-public class Lab_10_ex1
-{
-	public static void main(String[]args)
-	{
-		Integer [] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79};
-		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
-		removePrimes(nums);
-	
-	}
-	
-	public static void removePrimes(Integer [] nums)
-	{
-		for(int i = 0; i <= num.size(); i++)
-		{
-			if(gFactor(nums[i]) = 0)
-			{
-				nums.remove(nums[i]);
-				i--;
-			}
-		}
-		
-		
-		
-	}
-	
-	
-	public static int gFactor(int n)
-	{
-		for(int i = 2; i <= n; i++)
-		{	
-			if(n % i == 0)
-			{
-				return 1;
-			}
-			
-			else
-			{
-				return 0;
-			}
-		}
-	}
-	
-	
-	
-}
->>>>>>> origin/master
